85da7beec2119944ed30e04650533ececd6fce7d
GobinB <gobindabastola6@gmail.com>
2023-11-29 21:50:11 -0500

    modfying old one

--- colors.py
+++
@@ -1,7 +0,0 @@
-import PySimpleGUI as sg
-
-# Set the theme
-sg.theme('LightBlue2')
-
-# Print theme's background color
-print(sg.LOOK_AND_FEEL_TABLE['LightBlue2']['BACKGROUND'])
---
+++ nav.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from turtle import left
+from pandas import DataFrame
+import pandas as pd1
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,NavigationToolbar2Tk)
+from tkinter import filedialog
+from tkinter import *
+from tkinter import ttk, filedialog
+from tkinter.filedialog import askopenfile
+
+class Navbar(tk.Frame): 
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.button1 = tk.Radiobutton(self, text="Button 1")
+        self.button2 = tk.Scale(self, from_=0, to=100, orient=HORIZONTAL)
+        self.button3 = tk.Button(self, text="Button 3")
+        self.button1.pack(side="top")
+        self.button2.pack(side="top")
+        self.button3.pack(side="top")
+    
+
+class Toolbar(tk.Frame): 
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
