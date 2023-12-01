f2c15c04da2b1481fa457d5c17331505bfb5a672
AhmedMurshid <ahmed.abdullahione@gmail.com>
2023-12-01 18:50:32 -0500

    check if this workd

--- test1.py
+++ test1.py
@@ -9,6 +9,8 @@ import matplotlib.dates as mdates
 import numpy as np
 import math
 from tkinter import ttk  
+import tkinter as tk
+
 
 # Constants
 BASE_DIRECTORY = 'Dataset'
@@ -25,54 +27,48 @@ def convert_to_local_time(df, timezone='UTC'):
     return df.reset_index()
 
 def generate_file_location(date, option):
+    base_directory = 'Dataset'
     formatted_date = date.replace('-', '')
-    return f"{BASE_DIRECTORY}/{formatted_date}/{option}/summary.csv"
+    file_location = f"{base_directory}/{formatted_date}/{option}/summary.csv"
+    return file_location
+
 
 def draw_figure(canvas, figure, loc=(0, 0)):
-    if hasattr(canvas, 'figure_agg'):
-        canvas.figure_agg.get_tk_widget().forget()
-        plt.close(canvas.figure_agg.figure)
-        # Remove the old scrollbars
-        for child in canvas.winfo_children():
-            if isinstance(child, ttk.Scrollbar):
-                child.destroy()
+    # First, destroy the previous figure and its frame to avoid duplication
+    if hasattr(canvas, 'scrollable_frame'):
+        canvas.scrollable_frame.destroy()
 
-    # Create a FigureCanvasTkAgg object
-    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    # Create a new frame to contain the canvas and scrollbar
+    scrollable_frame = tk.Frame(canvas.master)
+    canvas.scrollable_frame = scrollable_frame
+    scrollable_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    # Create the FigureCanvasTkAgg object within the scrollable frame
+    figure_canvas_agg = FigureCanvasTkAgg(figure, master=scrollable_frame)
     figure_canvas_agg.draw()
+    widget = figure_canvas_agg.get_tk_widget()
+    widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-    # Pack the FigureCanvasTkAgg widget to fill both the X and Y axes
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-
-    # Create vertical scrollbar and attach it to the canvas
-    vscrollbar = ttk.Scrollbar(canvas, orient='vertical', command=canvas.yview)
+    # Create vertical scrollbar and attach it to the widget within the frame
+    vscrollbar = ttk.Scrollbar(scrollable_frame, orient='vertical', command=widget.yview)
     vscrollbar.pack(side='right', fill='y')
-    canvas.configure(yscrollcommand=vscrollbar.set)
 
-    # Create horizontal scrollbar and attach it to the canvas
-    hscrollbar = ttk.Scrollbar(canvas, orient='horizontal', command=canvas.xview)
-    hscrollbar.pack(side='bottom', fill='x')
-    canvas.configure(xscrollcommand=hscrollbar.set)
+    # Configure the canvas widget's yscrollcommand to the scrollbar
+    widget.configure(yscrollcommand=vscrollbar.set)
 
-    # Set the canvas scrolling region
-    canvas.config(scrollregion=canvas.bbox("all"))
-
+    # Update canvas.figure_agg
     canvas.figure_agg = figure_canvas_agg
 
-# Function to handle mouse scroll events for zooming
-def on_scroll(event):
-    ax = event.inaxes
-    if ax is None:
-        return
-    fig = ax.get_figure()
-    current_ylim = ax.get_ylim()
-    scale_factor = 1.2 if event.button == 'up' else 1 / 1.2  # Adjust the zoom level as needed
-    new_ylim = [x * scale_factor for x in current_ylim]
-    ax.set_ylim(new_ylim)
-    fig.canvas.draw_idle()
+
+
 
 def plot_data(file_location, columns, chart_type, timezone):
-    df = pd.read_csv(file_location)
+    try:
+        df = pd.read_csv(file_location)
+    except FileNotFoundError:
+        print(f"File not found: {file_location}")
+        return None
+
     df = convert_to_local_time(df, timezone)
 
     # Assuming you want to plot these attributes based on checkbox selections
@@ -93,49 +89,38 @@ def plot_data(file_location, columns, chart_type, timezone):
     num_rows = int(math.ceil(num_attributes / 2))
     num_cols = min(2, num_attributes)
 
-    fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 3 * num_rows), sharex=False, squeeze=False)
+    fig, axs = plt.subplots(num_attributes, 1, figsize=(15, 3 * num_rows), sharex=False)
+
+    if num_attributes == 1:
+        axs = np.array([axs])  # Make sure axs is always a numpy array, even for a single subplot
 
     current_subplot = 0  # Track the current subplot index
 
-    def on_scroll(event):
-        # Implement your zooming logic here
-        pass
-
     for i, (attr, is_checked) in enumerate(attributes_to_plot):
         if is_checked:
-            row = current_subplot // num_cols
-            col = current_subplot % num_cols
-            ax = axs[row, col]
-
-            x = df['Datetime (UTC)']
-            y = df[attr]
+            ax = axs[current_subplot]
+            current_subplot += 1
 
             if chart_type == 'plot':
-                ax.plot(x, y, label=attr)
+                ax.plot(df['Datetime (UTC)'], df[attr], label=attr)
             elif chart_type == 'scatter':
-                ax.scatter(x, y, label=attr)
+                ax.scatter(df['Datetime (UTC)'], df[attr], label=attr)
             elif chart_type == 'bar':
-                ax.bar(x, y, label=attr)
+                ax.bar(df['Datetime (UTC)'], df[attr], label=attr)
 
             ax.set_ylabel(attr)
             ax.legend()
 
-            current_subplot += 1
-
-    for i in range(current_subplot, num_rows * num_cols):
-        for ax in axs.flat:
-            ax[i].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
+    for ax in axs:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
 
     # Remove unused subplots
-    for i in range(current_subplot, num_rows * num_cols):
-        fig.delaxes(axs.flat[i])
+    for i in range(current_subplot, num_attributes):
+        fig.delaxes(axs[i])
 
     fig.tight_layout()
-
-    # Connect the 'motion_notify_event' to the scroll function
-    fig.canvas.mpl_connect('motion_notify_event', on_scroll)
-
     return fig
+
 # Data Handling Class
 class DataHandler:
     def __init__(self, base_directory=BASE_DIRECTORY):
@@ -162,6 +147,27 @@ def show_statistics(df):
     layout = [[sg.Text(str(stats))]]
     sg.Window('Statistics', layout, modal=True).read(close=True)
 
+def get_date_range(start_date, end_date):
+    """Generate a list of dates between start_date and end_date."""
+    start = pd.to_datetime(start_date)
+    end = pd.to_datetime(end_date)
+    date_range = pd.date_range(start, end)
+    return date_range
+
+def aggregate_data_from_range(start_date, end_date, selected_option, timezone):
+    """Load and combine data from a range of dates."""
+    date_range = get_date_range(start_date, end_date)
+    data_frames = []
+    for single_date in date_range:
+        file_location = generate_file_location(single_date.strftime('%Y-%m-%d'), selected_option)
+        try:
+            df = pd.read_csv(file_location)
+            df = convert_to_local_time(df, timezone)
+            data_frames.append(df)
+        except FileNotFoundError:
+            print(f"No data found for {single_date.strftime('%Y-%m-%d')}")
+    return pd.concat(data_frames) if data_frames else None
+
 def main():
     sg.theme('LightBlue2')
     chart_types = ['plot', 'scatter', 'bar']
@@ -171,27 +177,40 @@ def main():
     layout = [
         [sg.Text('Welcome')],
         [
+            sg.Checkbox('Enable Date Range', key='-ENABLERANGE-', enable_events=True),
+        ],
+        [
             sg.Text('Select Date:'),
-            sg.Combo(date_combobox_choices, key='-DATE-', default_value='None')
+            sg.Combo(date_combobox_choices, key='-DATE-', default_value='None'),
+            sg.Text('Or Select Date Range:'),
+            sg.Text('Start Date:'),
+            sg.Input(key='-STARTDATE-', enable_events=True, size=(20,1)),
+            sg.CalendarButton('Choose Start Date', target='-STARTDATE-', key='-STARTDATE-BTN-', format='%Y-%m-%d'),
+            sg.Text('End Date:'),
+            sg.Input(key='-ENDDATE-', enable_events=True, size=(20,1)),
+            sg.CalendarButton('Choose End Date', target='-ENDDATE-', key='-ENDDATE-BTN-', format='%Y-%m-%d')
         ],
         [
             sg.Text('Select Option:'),
             sg.Combo(option_combobox_choices, key='-OPTION-', default_value='310')
-        ],  # Modified to use a combo box for selecting options
+        ],
         [
             sg.Checkbox('Acc magnitude avg', key='-ACC-', default=True),
             sg.Checkbox('Eda avg', key='-EDA-', default=True),
             sg.Checkbox('Temp avg', key='-TEMP-', default=True),
             sg.Checkbox('Movement intensity', key='-MOVEMENT-', default=True),
-            sg.Checkbox('On Wrist', key='-ON WRIST-')  # Adding checkbox for On Wrist data
+            sg.Checkbox('On Wrist', key='-ON WRIST-')
         ],
         [sg.Text('Chart Type:'), sg.Combo(chart_types, key='-CHART TYPE-', default_value='plot')],
-        [sg.Text('Select Timezone:'), sg.Combo(TIMEZONES, default_value='UTC', key='-TIMEZONE-')],  # Added timezone selection
-        [sg.Button('Show Graph'), sg.Button('Show Statistics')],  # Added Show Statistics button
+        [sg.Text('Select Timezone:'), sg.Combo(TIMEZONES, default_value='UTC', key='-TIMEZONE-')],
+        [sg.Button('Show Graph'), sg.Button('Show Statistics')],
         [sg.Canvas(key='-CANVAS-')]
     ]
 
-    window = sg.Window('Data Analysis App', layout)
+
+    #window = sg.Window('Data Analysis App', layout)
+    window = sg.Window('Data Analysis App', layout, size=(1400, 950), resizable=True)
+
     current_timezone = 'UTC'  # Default time zone
 
     while True:
@@ -199,14 +218,36 @@ def main():
         if event == sg.WINDOW_CLOSED:
             break
 
+        if event == '-ENABLERANGE-':
+            enable_range = values['-ENABLERANGE-']
+            window['-STARTDATE-'].update(disabled=not enable_range)
+            window['-STARTDATE-BTN-'].update(disabled=not enable_range)
+            window['-ENDDATE-'].update(disabled=not enable_range)
+            window['-ENDDATE-BTN-'].update(disabled=not enable_range)
+            window['-DATE-'].update(disabled=enable_range)
+
         if event == 'Show Graph':
             selected_date = values['-DATE-']
+            start_date = values['-STARTDATE-']
+            end_date = values['-ENDDATE-']
             selected_option = values['-OPTION-']
 
-            if selected_date and selected_option:
-                file_location = generate_file_location(selected_date, selected_option)
-                fig = plot_data(file_location, values, values['-CHART TYPE-'], current_timezone)
-                draw_figure(window['-CANVAS-'].TKCanvas, fig)
+            if (selected_date or (start_date and end_date)) and selected_option:
+                if start_date and end_date:
+                    aggregated_data = aggregate_data_from_range(start_date, end_date, selected_option, current_timezone)
+                    if aggregated_data is not None:
+                        fig = plot_data(generate_file_location(start_date, selected_option), values, values['-CHART TYPE-'], current_timezone)
+                        draw_figure(window['-CANVAS-'].TKCanvas, fig)
+                    else:
+                        sg.popup('No data available for the selected date range.')
+                elif selected_date:
+                    file_location = generate_file_location(selected_date, selected_option)
+                    fig = plot_data(file_location, values, values['-CHART TYPE-'], current_timezone)
+                    if fig is not None:
+                        draw_figure(window['-CANVAS-'].TKCanvas, fig)
+                    else:
+                        sg.popup(f'No data found for the selected date: {selected_date}')
+
 
         if event == 'Show Statistics':
             selected_date = values['-DATE-']
