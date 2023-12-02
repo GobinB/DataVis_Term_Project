b88da7e9721b71f15008a72dc115b42e29b73520
AhmedMurshid <ahmed.abdullahione@gmail.com>
2023-12-01 20:58:31 -0500

    update

--- new_creation.py
+++ new_creation.py
@@ -59,26 +59,10 @@ def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg.draw()
     widget = figure_canvas_agg.get_tk_widget()
     widget.pack(side='top', fill='both', expand=True)
-
-    # Update idletasks before configuring the scrollregion
     widget.update_idletasks()
 
-    # Calculate the figure size in pixels, which is needed for the scrollregion
-    dpi = figure.get_dpi()
-    fig_width, fig_height = figure.get_size_inches()
-    scrollregion = (0, 0, fig_width * dpi, fig_height * dpi)
 
-    # Set the scroll region to the size of the figure in pixels
-    canvas.configure(scrollregion=scrollregion)
-
-    # Create scrollbars and attach them to the figure canvas
-    vscrollbar = ttk.Scrollbar(canvas, orient='vertical', command=widget.yview)
-    vscrollbar.pack(side='right', fill='y')
-    widget.configure(yscrollcommand=vscrollbar.set)
-
-    hscrollbar = ttk.Scrollbar(canvas, orient='horizontal', command=widget.xview)
-    hscrollbar.pack(side='bottom', fill='x')
-    widget.configure(xscrollcommand=hscrollbar.set)
+   
 
     canvas.figure_agg = figure_canvas_agg
 
@@ -266,9 +250,23 @@ def main():
 
     # Initialize with a default value
     # selected_timezone_var = sg.StringVar(value=selected_timezone)
+    canvas_layout = [
+            [sg.Canvas(key='-CANVAS-')],
+            [sg.Button('Zoom In'), sg.Button('Zoom Out'), sg.Button('Reset Zoom')]
+        ]
+    # Create a frame for the canvas and other elements
+    canvas_frame = sg.Frame('Plot', layout=canvas_layout, title_color='blue')
 
+    # Create a slider to enable vertical scrolling
+    scroll_slider = sg.Slider(range=(0, 200), default_value=-100, orientation='v', size=(15, 20), key='-SCROLL-')
+
+    # Create a column to hold the canvas frame and the slider
+    column_layout = [
+        [canvas_frame, scroll_slider]
+    ]
     layout = [
         [sg.Text('Welcome')],
+       
         [
             sg.Checkbox('Enable Date Range', key='-ENABLERANGE-', enable_events=True),
         ],
@@ -300,7 +298,9 @@ def main():
         [sg.Text('Select Timezone:'), sg.Combo(TIMEZONES, default_value=selected_timezone, key='-TIMEZONE-', enable_events=True)],
 
         [sg.Button('Show Graph'), sg.Button('Show Statistics'), sg.Button('Open Time Box')], 
-        [sg.Canvas(key='-CANVAS-')],
+        # [sg.Canvas(key='-CANVAS-')],
+        [sg.Column(column_layout, size=(900, 750), scrollable=True)],
+
         [sg.Button('Zoom In'), sg.Button('Zoom Out'), sg.Button('Reset Zoom')]
 
     ]
